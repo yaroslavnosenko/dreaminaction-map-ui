cfc61721568c74ddf6f8dd6fd9dccb620439464c
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2024-05-18 16:52:14 -0400

    Sign in page

---
+++ app/auth/page.tsx
@@ -0,0 +1,77 @@
+import {
+  Anchor,
+  Box,
+  Button,
+  Center,
+  Divider,
+  Text,
+  Title,
+} from '@mantine/core'
+import Image from 'next/image'
+import Link from 'next/link'
+import { FaFacebook, FaGoogle } from 'react-icons/fa'
+import { MdArrowBack } from 'react-icons/md'
+
+export default function Auth() {
+  return (
+    <Center mih="100vh" p="lg">
+      <Box maw="24rem">
+        <Anchor component={Link} href="/" w={56}>
+          <Image
+            className="animated"
+            priority
+            src="/logo.svg"
+            height={56}
+            width={56}
+            alt={'logo'}
+          />
+        </Anchor>
+        <Title mt="md" order={1}>
+          Sign in
+        </Title>
+        <Box my="2xl">
+          <Button
+            leftSection={<FaGoogle />}
+            w="100%"
+            color="red"
+            size="lg"
+            radius="xl"
+          >
+            Sign in with Google
+          </Button>
+          <Divider my="md" label="or" labelPosition="center" />
+          <Button
+            leftSection={<FaFacebook />}
+            w="100%"
+            size="lg"
+            radius="xl"
+            color="blue"
+          >
+            Sign in with Facebook
+          </Button>
+        </Box>
+        <Text>
+          By creating an account you agree with our{' '}
+          <Anchor c="green" component={Link} href="/">
+            Terms of Service
+          </Anchor>{' '}
+          and{' '}
+          <Anchor c="green" component={Link} href="/">
+            Privacy Policy
+          </Anchor>
+          .
+        </Text>
+        <Button
+          px={0}
+          leftSection={<MdArrowBack size={24} />}
+          mt="xl"
+          href={'/'}
+          component={Link}
+          variant="transparent"
+        >
+          Back to Map
+        </Button>
+      </Box>
+    </Center>
+  )
+}
--- components/map/header.tsx
+++ components/map/header.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex } from '@mantine/core'
+import { Anchor, Button, Flex } from '@mantine/core'
 import Image from 'next/image'
 import Link from 'next/link'
 import { MdOutlineKey } from 'react-icons/md'
@@ -6,14 +6,23 @@ import { MdOutlineKey } from 'react-icons/md'
 export const Header = () => {
   return (
     <Flex justify="space-between" align="center">
-      <Link href="/">
-        <Image priority src="/logo.svg" height={56} width={56} alt={'logo'} />
-      </Link>
+      <Anchor component={Link} href="/" w={56}>
+        <Image
+          className="animated"
+          priority
+          src="/logo.svg"
+          height={56}
+          width={56}
+          alt={'logo'}
+        />
+      </Anchor>
       <Button
+        component={Link}
         color="black"
         variant="filled"
         radius="xl"
         size="md"
+        href="/auth"
         leftSection={<MdOutlineKey size={24} />}
       >
         Sign In
