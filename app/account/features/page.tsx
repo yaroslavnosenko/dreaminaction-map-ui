4dd91a96075837f5c6b975081e8bdd61eefe2b0e
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2024-07-08 18:55:57 -0400

    features fetch

--- app/account/features/page.tsx
+++ app/account/features/page.tsx
@@ -1,48 +1,67 @@
-import { DStack } from '@/components/ui'
-import { features } from '@/mocks'
-import { ActionIcon, Box, Button, Group, Title } from '@mantine/core'
+'use client'
+
+import { gql, useQuery } from '@apollo/client'
+import { Anchor, Box, Button, Group, Title } from '@mantine/core'
 import Link from 'next/link'
-import { MdAdd, MdEdit } from 'react-icons/md'
+import { MdAdd } from 'react-icons/md'
+
+import { DStack } from '@/components/ui'
+import { jql } from '@/utils'
+
+import { useMe } from '@/hooks'
+import { Query, UserRole } from '@/types'
+
+const query = gql(
+  jql({
+    query: {
+      features: {
+        id: true,
+        name: true,
+      },
+    },
+  })
+)
 
 export default function Features() {
+  const me = useMe()
+  const { data, loading } = useQuery<Query>(query)
+
+  const features = data?.features || []
+  const isAdmin = me?.role === UserRole.Admin
+
+  if (loading) {
+    return <>Loading</>
+  }
   return (
     <Box>
       <Group h={56} mb="2xl" justify="space-between">
         <Title order={2}>Features</Title>
-        <Button
-          component={Link}
-          size="md"
-          radius="xl"
-          leftSection={<MdAdd size={24} />}
-          href="/account/features/new"
-          className="animated"
-        >
-          New
-        </Button>
+        {isAdmin && (
+          <Button
+            component={Link}
+            size="md"
+            radius="xl"
+            leftSection={<MdAdd size={24} />}
+            href="/account/features/new"
+            className="animated"
+          >
+            New
+          </Button>
+        )}
       </Group>
       <Title fw="normal" order={4} opacity={0.7}>
         10 items
       </Title>
       <Box h={1} bg="#f1f1f1" my="xl" />
-      <DStack divider={<Box h={1} bg="#f1f1f1" />} gap="sm">
+      <DStack divider={<Box h={1} bg="#f1f1f1" />} gap="md">
         {features.map(({ id, name }) => (
-          <Group
-            mih={48}
-            gap={0}
+          <Anchor
+            component={Link}
             key={id}
-            justify="space-between"
-            wrap="nowrap"
+            href={isAdmin ? '/account/features/' + id : '#'}
           >
-            <Box flex="1">{name}</Box>
-            <ActionIcon
-              href={'/account/features/' + id}
-              component={Link}
-              variant="white"
-              size="xl"
-            >
-              <MdEdit size={24} />
-            </ActionIcon>
-          </Group>
+            <Title order={4}>{name}</Title>
+          </Anchor>
         ))}
       </DStack>
     </Box>
